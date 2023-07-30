cffb8c1c228d5121dad241efd8e372530a9f152b
UncleP04 <philipnssien@gmail.com>
2023-07-30 15:01:45 -0700

    update remove screen height

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,14 +6,14 @@ import Logo from '../../public/Images/logo.png'
 const Header: React.FC = () => {
   return (
     <div>
-      <section className="relative w-[100%] flex justify-center align-middle lg:h-auto h-[100vh] overflow-x-hidden bg-primary">
-        {/* <Image
+      <section className="relative w-[100%] flex justify-center align-middle h-auto overflow-x-hidden bg-primary">
+        <Image
           src={BackgroundImg}
           alt="backImage.png"
           width={700}
           height={300}
           className="lg:w-[400px] w-[300px] lg:h-[600px] h-[500px] fixed top-0 lg:left-[30%] left-0"
-        /> */}
+        />
 
         <div className="mt-[15%] lg:mt-[3%] z-10">
           <span className="flex justify-center align-center" data-aos="zoom-in" data-aos-duration="1000">
